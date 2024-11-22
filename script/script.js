89a4278c8938c9deb654167243829f242ff1bf63
Edison R. <edison@correo.com>
2024-11-22 14:00:29 -0500

    Funcionalidad Boton Resetear y Control de Boton 0

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 let divBotones = document.querySelector("#botones");
-console.log(divBotones);
-
+// console.log(divBotones);
 let pantalla = document.querySelector("#pantalla");
+
+let boton_resetear = document.querySelector("#resetear");
 
 let primerNumero = true;
 let limite_caracteres_pantalla = 10;
@@ -17,13 +18,20 @@ function accionNumeros(texto) {
     // alert('Haz dado click en el número ' + texto);
     if (pantalla.innerText.length < limite_caracteres_pantalla) {
         if (primerNumero) {
-            pantalla.innerText = texto;
-            primerNumero = false;
+            if (texto != '0') {
+                pantalla.innerText = texto;
+                primerNumero = false;
+            }
         } else {
             pantalla.innerText = pantalla.innerText + texto;
         }
     }
 }
+
+boton_resetear.addEventListener('click', () => {
+    pantalla.innerHTML = '0';
+    primerNumero = true;
+});
 
 
 
