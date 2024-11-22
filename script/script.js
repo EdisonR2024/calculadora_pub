f0e8e23fabe9d167b4b358220a30ee39a1f2878d
Edison R. <edison@correo.com>
2024-11-21 21:43:33 -0500

    Funcionalidad en Botones de Números

--- script/script.js
+++ script/script.js
@@ -1,18 +1,28 @@
-let divBotones=document.querySelector("#botones");
+let divBotones = document.querySelector("#botones");
 console.log(divBotones);
 
-let pantalla=document.querySelector("#pantalla");
+let pantalla = document.querySelector("#pantalla");
 
-divBotones.addEventListener('click', (event)=>{
+let primerNumero = true;
+let limite_caracteres_pantalla = 10;
+
+divBotones.addEventListener('click', (event) => {
     let elemento_btn = event.target;
     if (elemento_btn.classList.contains('numero')) {
         accionNumeros(elemento_btn.innerText);
     }
 });
 
-function accionNumeros(texto){
+function accionNumeros(texto) {
     // alert('Haz dado click en el número ' + texto);
-    pantalla.innerText=texto;
+    if (pantalla.innerText.length < limite_caracteres_pantalla) {
+        if (primerNumero) {
+            pantalla.innerText = texto;
+            primerNumero = false;
+        } else {
+            pantalla.innerText = pantalla.innerText + texto;
+        }
+    }
 }
 
 
