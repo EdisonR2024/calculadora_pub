fe4c61a15a720b3ed89f104788eff4a6fdcb3f8b
Edison R. <edison@correo.com>
2024-12-09 21:33:50 -0500

    Se agrega el boton de borrar y su funcionalidad

--- script/script.js
+++ script/script.js
@@ -4,14 +4,35 @@ let pantalla = document.querySelector("#pantalla");
 
 let boton_resetear = document.querySelector("#resetear");
 
+let boton_punto = document.querySelector("#punto");
+
+let boton_borrar = document.querySelector("#borrar");
+
 let primerNumero = true;
+let punto_activado = false;
 let limite_caracteres_pantalla = 10;
+
 
 divBotones.addEventListener('click', (event) => {
     let elemento_btn = event.target;
     if (elemento_btn.classList.contains('numero')) {
         accionNumeros(elemento_btn.innerText);
     }
+});
+
+boton_resetear.addEventListener('click', () => {
+    resetear();
+});
+
+boton_punto.addEventListener('click', () => {
+    if (punto_activado === false) {
+        if (primerNumero) {
+            accionNumeros('0.');
+        } else {
+            accionNumeros('.');
+        }
+    }
+    punto_activado = true;
 });
 
 function accionNumeros(texto) {
@@ -28,11 +49,23 @@ function accionNumeros(texto) {
     }
 }
 
-boton_resetear.addEventListener('click', () => {
+function resetear() {
     pantalla.innerHTML = '0';
     primerNumero = true;
-});
+    punto_activado = false;
+}
 
+boton_borrar.addEventListener('click', () => {
+    if (pantalla.innerText.length > 1) {
+        pantalla.innerText = pantalla.innerText.slice(0, -1);
+        let caracterBorrar = pantalla.innerText.slice(-1);
+        if (caracterBorrar === '.') {
+            punto_activado = false;
+        }
+    } else {
+        resetear();
+    }
+});
 
 
 
