3474c651907892160dccceb7114fa9f365e42b2b
Edison R. <edison@correo.com>
2024-12-11 17:30:48 -0500

    Se agrega otra pantalla para ver las operaciones y funcionalidades iniciales para presentar en la otra pantalla al dar click en los operadores

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 let divBotones = document.querySelector("#botones");
 // console.log(divBotones);
 let pantalla = document.querySelector("#pantalla");
+let pantalla_operaciones = document.querySelector("#pantalla_operaciones");
 
 let boton_resetear = document.querySelector("#resetear");
 
@@ -8,20 +9,35 @@ let boton_punto = document.querySelector("#punto");
 
 let boton_borrar = document.querySelector("#borrar");
 
+// let boton_operadores= document.querySelectorAll('.operadores');
+
 let primerNumero = true;
 let punto_activado = false;
 let limite_caracteres_pantalla = 10;
+
+let numeroActual=0;
+let numeroAnterior=0;
+let operadorSeleccionado='';
 
 
 divBotones.addEventListener('click', (event) => {
     let elemento_btn = event.target;
     if (elemento_btn.classList.contains('numero')) {
         accionNumeros(elemento_btn.innerText);
+    }else if(elemento_btn.classList.contains('operadores')){
+        // alert('Click en Operadores');
+        // console.log(elemento_btn.innerText);
+        let operador=elemento_btn.innerText;
+        pantalla_operaciones.innerText=pantalla.innerText+operador;
+        pantalla_operaciones.classList.remove('po_color');
+        numeroAnterior=numeroActual;
+
+        resetearValores();
     }
 });
 
 boton_resetear.addEventListener('click', () => {
-    resetear();
+    reseteoTotal();
 });
 
 boton_punto.addEventListener('click', () => {
@@ -42,18 +58,33 @@ function accionNumeros(texto) {
             if (texto != '0') {
                 pantalla.innerText = texto;
                 primerNumero = false;
+                numeroActual=pantalla.innerText;
+                console.log(numeroActual);
             }
         } else {
             pantalla.innerText = pantalla.innerText + texto;
+            numeroActual=pantalla.innerText;
+            console.log(numeroActual);
         }
     }
 }
 
-function resetear() {
+function reseteoTotal(){
+    resetearPantalla();
+    resetearValores();
+}
+
+function resetearPantalla() {
     pantalla.innerHTML = '0';
+    pantalla_operaciones.innerText='0';
+    pantalla_operaciones.classList.add('po_color');
+}
+
+function resetearValores(){
     primerNumero = true;
     punto_activado = false;
 }
+
 
 boton_borrar.addEventListener('click', () => {
     if (pantalla.innerText.length > 1) {
@@ -63,9 +94,13 @@ boton_borrar.addEventListener('click', () => {
             punto_activado = false;
         }
     } else {
-        resetear();
+        reseteoTotal();
     }
 });
+
+// boton_operadores.forEach.addEventListener('click',()=>{
+//     alert('Click en Operadores');
+// });
 
 
 
